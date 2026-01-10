73a8bec33fa76cef197337b1729ea00be2d2423b
Tado Hopsky <frolov.dev1375@gmail.com>
2026-01-10 21:50:31 +0300

    Переход на ООП

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ShippingCalculator sc = new ShippingCalculator();
         sc.getFirstInfoForUser();
 
-        int cargo = sc.getCargoFromUser(scanner);
-        int distance = sc.getDistanceFromUser(scanner);
+        while (true){
+            try{
+                int cargo = sc.getCargoFromUser(scanner);
+                int distance = sc.getDistanceFromUser(scanner);
 
-        System.out.println(sc.calculatePrice(cargo, distance));
+                if(cargo <= 0 || distance <= 0){
+                    System.err.println("Ошибка: данные должны быть положительными");
+                    continue;
+                }
+                Cargo cargoModel = new Cargo(cargo, distance);
+
+                System.out.println("Расчет для " + cargoModel + ": " + sc.calculatePrice(cargoModel));
+            }catch (NumberFormatException e){
+                System.err.println("Ошибка вода от пользователя\nОжидается число\n" + e);
+            }
+
+            System.out.println("Хотите рассчитать еще одну доставку? (yes/no)");
+            String userInput = scanner.nextLine();
+
+            if(!userInput.equalsIgnoreCase("yes")){
+                break;
+            }
+        }
+        scanner.close();
     }
 }
