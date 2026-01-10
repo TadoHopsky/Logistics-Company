b7f3585b3648106bddd9fb5ed676d0bc4dc0ab20
Tado Hopsky <frolov.dev1375@gmail.com>
2026-01-10 21:22:55 +0300

    Обработка ошибок и Циклы

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         ShippingCalculator sc = new ShippingCalculator();
         sc.getFirstInfoForUser();
 
-        int cargo = sc.getCargoFromUser(scanner);
-        int distance = sc.getDistanceFromUser(scanner);
+        while (true){
+            try{
+                int cargo = sc.getCargoFromUser(scanner);
+                int distance = sc.getDistanceFromUser(scanner);
 
-        System.out.println(sc.calculatePrice(cargo, distance));
+                if(cargo <= 0 || distance <= 0){
+                    System.err.println("Ошибка: данные должны быть положительными");
+                    continue;
+                }
+
+                System.out.println(sc.calculatePrice(cargo, distance));
+            }catch (NumberFormatException e){
+                System.err.println("Ошибка вода от пользователя\nОжидается число\n" + e);
+            }
+
+            System.out.println("Хотите рассчитать еще одну доставку? (yes/no)");
+            String userInput = scanner.nextLine();
+
+            if(!userInput.equalsIgnoreCase("yes")){
+                break;
+            }
+        }
+        scanner.close();
     }
 }
